c28d4f7dd4a0dd2f50c237847fa02ce3b698cd88
corex <33293384+Ridascxz@users.noreply.github.com>
2021-07-11 15:12:47 +0300

    Add files via upload

---
+++ clock.sh
@@ -0,0 +1,111 @@
+#!bin/sh
+#! Time
+echo 14:23 21°C
+wait 3
+cls
+
+#! Animation
+echo H
+wait 0.25
+clear
+echo HE
+wait 0.25
+clear
+echo HEL
+wait 0.25
+clear
+echo HELL
+wait 0.25
+clear
+echo HELLO
+wait 0.25
+clear
+echo HELLO 
+wait 0.25
+clear
+echo HELLO M
+wait 0.25
+clear
+echo HELLO MA
+wait 0.25
+clear
+echo HELLO MAH
+wait 0.25
+clear
+echo HELLO MAH 
+wait 0.25
+clear
+echo HELLO MAH B
+wait 0.25
+clear
+echo HELLO MAH BR
+wait 0.25
+clear
+echo HELLO MAH BRU
+wait 0.25
+clear
+echo HELLO MAH BRUD
+wait 0.25
+clear
+echo HELLO MAH BRUDD
+wait 0.25
+clear
+echo HELLO MAH BRUDDA
+wait 0.25
+clear
+echo HELLO MAH BRUDDA
+wait 0.25
+clear
+echo HELLO MAH BRUDDA
+wait 0.25
+clear
+echo HELLO MAH BRUDDA
+wait 0.25
+clear
+echo HELLO MAH BRUDDA
+wait 0.25
+clear
+echo HELLO MAH BRUDDA
+wait 0.25
+clear
+echo HELLO MAH BRUDD
+wait 0.25
+clear
+echo HELLO MAH BRUD
+wait 0.25
+clear
+echo HELLO MAH BR
+wait 0.25
+clear
+echo HELLO MAH B
+wait 0.25
+clear
+echo HELLO MAH 
+wait 0.25
+clear
+echo HELLO MA
+wait 0.25
+clear
+echo HELLO M
+wait 0.25
+clear
+echo HELLO 
+wait 0.25
+clear
+echo HELL
+wait 0.25
+clear
+echo HEL
+wait 0.25
+clear
+echo HE
+wait 0.25
+clear
+echo H
+wait 0.25
+clear
+
+
+#! After Animation
+echo FEB 11
+wait 3.25
